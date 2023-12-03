7b83b6a3cb0de209ac1ae94a1b87f5ed37d4e62d
vishnumoger <vishnumoger@gmail.com>
2023-12-03 17:45:52 +0530

    test api

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
+app.get('/api/users', function (req, res) {
   res.send('Hello test app123');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000');
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000');
 });
